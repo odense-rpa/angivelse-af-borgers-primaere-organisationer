81313e471ca3a58e7fcb7e2aaf49d78f00648427
Jakob Kirkegaard Wibroe <jkwibroe91@gmail.com>
2025-03-27 10:01:22 +0100

    Tilføjet godkendte organisationer som en ny fil og refaktoreret kode til at bruge denne liste i stedet for at læse fra en Excel-fil + samt andre ting gjort pænere.

--- main.py
+++ main.py
@@ -2,30 +2,32 @@ import asyncio
 import logging
 import sys
 
-from odk_tools.tracking import Tracker
-from automation_server_client import AutomationServer, Workqueue, WorkItemError, Credential
-from kmd_nexus_client import NexusClient, CitizensClient, OrganizationsClient
 import pandas as pd
+
+from odk_tools.tracking import Tracker
+from automation_server_client import (
+    AutomationServer,
+    Workqueue,
+    WorkItemError,
+    Credential,
+)
+from kmd_nexus_client import NexusClient, CitizensClient, OrganizationsClient
+# temp fix since no Q:\
+from organizations import approved_organizations
 
 nexus_client = None
 citizens_client = None
 organizations_client = None
+tracking_client = None
 
-# Path to the file containing the list of approved organizations
-file_path_approved_organizations = r"Q:\RPA\Leverancer\Visitering til Lysningen\Liste over organisationer som muligvis skal have besked.xlsx"
-approved_organizations = pd.read_excel(file_path_approved_organizations)
-# Convert only the first column to a dictionary
-tmp = [value for (_,value) in approved_organizations.iloc[:, 0].to_dict().items()]
 
 async def populate_queue(workqueue: Workqueue):
     logger = logging.getLogger(__name__)
 
-    logger.info("Hello from populate workqueue!")
-
     all_organizations = organizations_client.get_organizations()
 
     for organization in all_organizations:
-        if organization["name"] not in tmp:
+        if organization["name"] not in approved_organizations:
             continue
 
         citizens = organizations_client.get_citizens_by_organization(organization)
@@ -33,49 +35,61 @@ async def populate_queue(workqueue: Workqueue):
         for citizen in citizens:
             data = {
                 "cpr": citizen["patientIdentifier"]["identifier"],
-                "organization": organization["name"]
+                "organization": organization["name"],
             }
 
             try:
                 # Add the item to the workqueue
-                workqueue.add_item(data,data["cpr"])
+                workqueue.add_item(data, data["cpr"])
             except Exception as e:
                 logger.error(f"Error adding item to workqueue: {data}. Error: {e}")
-    
-
-
 
 
 async def process_workqueue(workqueue: Workqueue):
     logger = logging.getLogger(__name__)
 
-    logger.info("Hello from process workqueue!")
-
-
-
     for item in workqueue:
         with item:
             data = item.get_data_as_dict()
             citizen = citizens_client.get_citizen(data["cpr"])
-            citizens_orgs = organizations_client.get_organizations_by_citizen(citizen=citizen)
-
-            # Find the organization that should be updated
-            updateable_organization = next((item for item in citizens_orgs if item["organization"]["name"] == data["organization"]), None)
-
-            # If the organization is already the primary organization, skip it
-            if updateable_organization["primaryOrganization"]:
-                continue
+            citizens_orgs = organizations_client.get_organizations_by_citizen(
+                citizen=citizen
+            )
 
             try:
+                # Find the organization that should be updated
+                updateable_organization = next(
+                    (
+                        item
+                        for item in citizens_orgs
+                        if item["organization"]["name"] == data["organization"]
+                    ),
+                    None,
+                )
+
+                # Citizen no longer has the organization
+                if not updateable_organization:
+                    continue
+
+                # If the organization is already the primary organization, skip it
+                if updateable_organization["primaryOrganization"]:
+                    continue
+
                 # Process the item here
-                organizations_client.update_citizen_organization_relationship(organization_relation=updateable_organization,endDate=None, primary_organization=True)
-                # Do some afregning eventually
-                database_client.track_task("Angivelse af borgers primære organisation")
+                organizations_client.update_citizen_organization_relationship(
+                    organization_relation=updateable_organization,
+                    endDate=None,
+                    primary_organization=True,
+                )
+
+                # Track the item
+                tracking_client.track_task("Angivelse af borgers primære organisation")
 
                 # Log
-                logger.info(f"Opdateret organisation som primær: {data["organization"]}")
+                logger.info(
+                    f"Opdateret organisation som primær: {data['organization']}"
+                )
 
-                pass
             except WorkItemError as e:
                 # A WorkItemError represents a soft error that indicates the item should be passed to manual processing or a business logic fault
                 logger.error(f"Error processing item: {data}. Error: {e}")
@@ -87,21 +101,28 @@ if __name__ == "__main__":
 
     workqueue = ats.workqueue()
 
-    # Initialize external systems for automation here..
+    # Path to the file containing the list of approved organizations
+    #file_path_approved_organizations = r"Q:\RPA\Leverancer\Visitering til Lysningen\Liste over organisationer som muligvis skal have besked.xlsx"
+    #approved_organizations_raw = pd.read_excel(file_path_approved_organizations)
+    # Convert only the first column to a dictionary
+    # approved_organizations = [
+    #     value for (_, value) in approved_organizations_raw.iloc[:, 0].to_dict().items()
+    # ]
+
+    # Initialize external systems
     credential = Credential.get_credential("KMD Nexus - produktion")
     tracking_credential = Credential.get_credential("Odense SQL Server")
 
     nexus_client = NexusClient(
-        instance= credential.get_data_as_dict()["instance"],
+        instance=credential.get_data_as_dict()["instance"],
         client_secret=credential.password,
-        client_id=credential.username
+        client_id=credential.username,
     )
     citizens_client = CitizensClient(nexus_client=nexus_client)
     organizations_client = OrganizationsClient(nexus_client=nexus_client)
 
-    database_client = Tracker(
-        username=tracking_credential.username, 
-        password=tracking_credential.password
+    tracking_client = Tracker(
+        username=tracking_credential.username, password=tracking_credential.password
     )
 
     # Queue management
